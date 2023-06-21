5ea8e2630249af5885b99b66ba08c4fae8016ab9
Pavan-tibil <pavan.kumar@tibilsolutions.com>
2023-06-21 20:34:26 +0530

    change the API end point and change the strings to constants

--- PortalBackend/Python/django/certificate/urls.py
+++ PortalBackend/Python/django/certificate/urls.py
@@ -1,10 +1,10 @@
 from rest_framework import routers
 from django.urls import path
-from .views import *
+from .views import Certificate,Verification
 
 
 router = routers.DefaultRouter()
 urlpatterns = [
     path('certificates', Certificate.as_view()),
-    path('verify_certificate/<str:osid>', VerifyCertificate.as_view()), 
+    path('verification/<str:certificate_id>', Verification.as_view()),
 ]
--- PortalBackend/Python/django/certificate/views.py
+++ PortalBackend/Python/django/certificate/views.py
@@ -1,16 +1,13 @@
-from configparser import ConfigParser
 import json
-from urllib.parse import urlencode
-
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from requests import request as rq
-from .constants import *
+from .constants import header,generate_token_payload,generate_token_url,certificate_header,get_request_method,post_request_method,data,Authorization,Bearer,signedCredentials,_osSignedData,access_token
+from .config_reader import read_config
 
-config = ConfigParser()
-config.read('/home/pavan/projects/Testing/vishwaas/certificate/config.ini')
 
+config = read_config()
 generate_token_url = config.get('URL', 'generate_token_url')
 certificate_url = config.get('URL', 'certificate_url')
 verify_certificate_url = config.get('URL', 'verify_certificate_url')
@@ -33,23 +30,20 @@ def call_api(**kwargs):
 
 
 def generate_tokens():
-    http_method = "POST"  
-    api_response = call_api(http_method=http_method, http_url=generate_token_url, http_header=header,
+    api_response = call_api(http_method=post_request_method, http_url=generate_token_url, http_header=header,
                             http_payload=generate_token_payload)
     return api_response
 
 
 def get_entity(access_token):
-    http_method = "GET"  
-    header['Authorization'] = "Bearer " + access_token  
-    certificate_response = call_api(http_method=http_method, http_url=certificate_url, http_header=header)
+    header[Authorization] = Bearer + access_token
+    certificate_response = call_api(http_method=get_request_method, http_url=certificate_url, http_header=header)
     return certificate_response
 
 
 def create_certificate(data, access_token):
-    http_method = "POST"  
-    certificate_header['Authorization'] = "Bearer " + access_token  
-    response = call_api(http_method=http_method, http_url=certificate_url, http_header=certificate_header,
+    certificate_header[Authorization] = Bearer + access_token
+    response = call_api(http_method=post_request_method, http_url=certificate_url, http_header=certificate_header,
                         http_payload=data)
     return response
 
@@ -57,51 +51,49 @@ def create_certificate(data, access_token):
 class Certificate(APIView):
     def post(self, request):
         get_token = generate_tokens()  
-        access_token = get_token['data']['access_token']  
-        get_the_entity = get_entity(access_token)
-        if isinstance(get_the_entity['data'], list):  
+        token = get_token[data][access_token]
+        get_the_entity = get_entity(token)
+        if isinstance(get_the_entity[data], list):
             flag = False
-            for i in get_the_entity['data']:  
+            for i in get_the_entity[data]:
                 if i['name'] == request.data['name'] and i['certificateid'] == request.data['certificateid'] and i[
                     'entitytype'] == request.data['entitytype'] and i['rebitversion'] == request.data[
                     'rebitversion'] and i['purposecode'] == request.data['purposecode'] and i['fitype'] == request.data[
-                    'fitype'] and i['expired'] == request.data['expired']:  
+                    'fitype'] and i['expired'] == request.data['expired']:
                     flag = True
                     break
             if flag:
                 return Response({'message': "Certificate already exists"}, status=status.HTTP_409_CONFLICT)
             elif flag == False:
-                response = create_certificate(json.dumps(request.data), access_token)
+                response = create_certificate(json.dumps(request.data), token)
                 return Response({"message": "Certificate created", "response": response}, status=status.HTTP_200_OK)
         elif get_the_entity['status_code'] == 404:
-            response = create_certificate(json.dumps(request.data), access_token)
+            response = create_certificate(json.dumps(request.data), token)
             return Response({"message": "Certificate created", "response": response}, status=status.HTTP_200_OK)
 
 
-def get_entity_by_id(osid, access_token):
-    http_method = "GET"  
-    certificate_header['Authorization'] = "Bearer " + access_token 
-    response = call_api(http_method=http_method, http_url=certificate_url + "/" + osid, http_header=certificate_header)
+def get_entity_by_id(certificate_id, access_token):
+    certificate_header[Authorization] = Bearer + access_token
+    response = call_api(http_method=get_request_method, http_url=certificate_url + "/" + certificate_id, http_header=certificate_header)
     return response
 
 
 def verify_certificate(access_token, payload):
-    http_method = "POST"
-    certificate_header['Authorization'] = "Bearer " + access_token 
-    response = call_api(http_method=http_method, http_url=verify_certificate_url, http_header=certificate_header,
+    certificate_header[Authorization] = Bearer + access_token
+    response = call_api(http_method=post_request_method, http_url=verify_certificate_url, http_header=certificate_header,
                         http_payload=payload)
     return response
 
 
-class VerifyCertificate(APIView):  
+class Verification(APIView):
 
-    def get(self, request, osid):
+    def get(self, request, certificate_id):
         get_token = generate_tokens()
-        access_token = get_token['data']['access_token']  
-        get_the_entity = get_entity_by_id(osid, access_token)
+        token = get_token[data][access_token]
+        get_the_entity = get_entity_by_id(certificate_id, token)
         unescaped_data = {
-            'signedCredentials': json.loads(get_the_entity['data']['_osSignedData'])  
+            signedCredentials : json.loads(get_the_entity[data][_osSignedData])
         }
         payload = json.dumps(unescaped_data)
-        response = verify_certificate(access_token, payload)
-        return Response({"Message:": "Certificate verified", "response": response['data']}, status=status.HTTP_200_OK)
+        response = verify_certificate(token, payload)
+        return Response({"Message:": "Certificate verified", "response": response[data]}, status=status.HTTP_200_OK)
