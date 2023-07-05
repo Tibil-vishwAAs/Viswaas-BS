e95b63fce53b54fdac15cea508c365eb0d3eeb08
TaherKIng <taher.makandar@tibilsolutions.com>
2023-07-05 18:21:49 +0530

    added running java code connecting with postgres

--- PortalBackend/Java/src/main/java/com/vishwaas/CertificateRepository.java
+++ PortalBackend/Java/src/main/java/com/vishwaas/CertificateRepository.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface CertificatesData extends JpaRepository<CertificateData, Integer>
+public interface CertificateRepository extends JpaRepository<CertificateData, Integer>
 {
 
 }
