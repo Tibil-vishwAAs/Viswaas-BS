f618d821fda783d26f9c59880fff7f4c62f7afd8
TaherKIng <taher.makandar@tibilsolutions.com>
2023-07-11 12:43:12 +0530

    added primary key to verification schemas file and removed unused variable

--- PortalBackend/schema/Schemas.sql
+++ PortalBackend/schema/Schemas.sql
@@ -6,7 +6,7 @@ created_by varchar(255),
 entitytype varchar(255), 
 expired varchar(255), 
 fitype varchar(255), 
-name varchar(255)j , 
+name varchar(255), 
 purposecode integer not null, 
 rebitversion varchar(255), 
 updated_at timestamp(6), 
@@ -16,17 +16,19 @@ primary key (id)
 
 
 CREATE TABLE IF NOT EXISTS Verification (
-  id UUID,
+  id integer PRIMARY KEY,
   name TEXT NOT NULL,
   entitytype TEXT NOT NULL,
   rebitversion TEXT NOT NULL,
-  purposecode INT NOT NULL,
+  purposecode integer NOT NULL,
+  fitype TEXT NOT NULL, 
   expired DATE,
   createdAt DATE,
   updatedAt DATE,
   updatedBy TEXT,
   createdBy TEXT,
-  certificate_id UUID,
-  CONSTRAINT fk_Certificates FOREIGN KEY (certificate_id) REFERENCES Certificates (certificate_id)
+  certificate_id integer,
+  CONSTRAINT fk_Certificates FOREIGN KEY (certificate_id) REFERENCES certificates (id)
 );
+
 
